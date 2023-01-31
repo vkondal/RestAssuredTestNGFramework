c4429fff914326ffd1737f3c94d89f933de71fd0
vkondal <shaber.kondal@gmail.com>
2023-01-31 18:25:26 +0530

    SCM web hook test

--- src/test/java/com/spotify/oauth2/api/applicationApi/demo.java
+++ src/test/java/com/spotify/oauth2/api/applicationApi/demo.java
@@ -4,5 +4,6 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("test jenkin trigger");
         System.out.println("test Jenkin polling to scm . ");
+        System.out.println("test Jenkin web hook testing to scm . ");
     }
 }
