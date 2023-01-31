41815da3bc19bf224febe393652facad468171e5
vkondal <shaber.kondal@gmail.com>
2023-01-31 18:08:43 +0530

    test jenkin polling scm

--- src/test/java/com/spotify/oauth2/api/applicationApi/demo.java
+++ src/test/java/com/spotify/oauth2/api/applicationApi/demo.java
@@ -3,5 +3,6 @@ package com.spotify.oauth2.api.applicationApi;
 public class demo {
     public static void main(String[] args) {
         System.out.println("test jenkin trigger");
+        System.out.println("test Jenkin polling to scm . ");
     }
 }
